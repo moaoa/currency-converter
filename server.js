0cb91326eea7fe500faf02d89d4785913b12df4d
moaad <moaadbn3@gmial.com>
2020-04-02 17:28:58 +0200

    'bakend

--- server.js
+++ server.js
@@ -4,31 +4,36 @@ const app = express()
 const port = process.env.PORT
 const key = process.env.KEY
 
-app.use(express.urlencoded({ extended: false }))
+const axios = require('axios')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 const url = 'https://fixer-fixer-currency-v1.p.rapidapi.com/'
 app.get('/currency-options', (req, res) => {
-    fetch(`${url}symbols`, {
-        headers: {
-            'x-rapidapi-host': 'fixer-fixer-currency-v1.p.rapidapi.com',
-            'x-rapidapi-key': key
-        },
-        method: 'GET'
-    })
-        .then(res => res.json())
-        .then(data => res.json(data))
+    axios
+        .get(`${url}symbols`, {
+            headers: {
+                'x-rapidapi-host': 'fixer-fixer-currency-v1.p.rapidapi.com',
+                'x-rapidapi-key': key
+            }
+        })
+        .then(data => res.json(Object.keys(data.data.symbols)))
+        .catch(err => console.log(err))
 })
-app.get('/rates:base', (req, res) => {
-    const base = req.params.base
-    fetch(`${url}latest?base=${base}`, {
-        headers: {
-            'x-rapidapi-host': 'fixer-fixer-currency-v1.p.rapidapi.com',
-            'x-rapidapi-key': key
-        },
-        method: 'GET'
-    })
-        .then(res => res.json())
-        .then(data => res.json(data))
+
+app.get('/rates/:base', (req, res) => {
+    let base = req.params.base
+
+    axios
+        .get(`${url}latest?base=${base}`, {
+            headers: {
+                'x-rapidapi-host': 'fixer-fixer-currency-v1.p.rapidapi.com',
+                'x-rapidapi-key': key
+            }
+        })
+        .then(data => res.json(data.data))
+        .catch(err => console.log(err))
 })
 
 app.listen(port || 5000)
